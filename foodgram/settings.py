e4be85b88d771b0d1ac7f281f843e1e9735f372b
Alexander Gryaznov <ne4istii@gmail.com>
2021-06-24 14:54:19 +0300

    fix misc tmplts

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -147,9 +147,15 @@ REST_FRAMEWORK = {
     ],
 }
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_POST = os.getenv('EMAIL_POST')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_POST = os.getenv('EMAIL_POST')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = 'ne4istii1@gmail.com'
+EMAIL_HOST_PASSWORD = 'n49Ma3yxT5'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
